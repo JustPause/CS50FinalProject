015b63d3ec8838ffc0ecf35b10ae38fd425f2477
JustPause <iamjuststan@hotmail.com>
2023-12-15 02:35:54 +0200

    AD

--- main.cpp
+++ main.cpp
@@ -1,74 +1,92 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdint.h>
 
 using std::string;
 
 class Encripsion
-{
-private:
-public:
-    static void openATheFileWithPassword()
     {
-    }
-};
+        public:
+
+        static void openATheFileWithPassword() {
+            std::cout << "Opens a file serech" << std::endl;
+            }
+
+        static uint32_t hash_string(const char* s) {
+            uint32_t hash = 0;
+
+            for (; *s; ++s)
+                {
+                hash += *s;
+                hash += (hash << 10);
+                hash ^= (hash >> 6);
+                }
+
+            hash += (hash << 3);
+            hash ^= (hash >> 11);
+            hash += (hash << 15);
+
+            return hash;
+            }
+    };
 
 class FileHandle
-{
-
-private:
-    string pathOfPassFile;
-
-    bool checkIfItsMyFile(string str)
     {
-        std::cout << str;
-        return true;
-    }
 
-    void inputCheck(string file)
-    {
-        std::ifstream ifile;
-        ifile.open(file);
+        private:
+        string pathOfPassFile;
 
-        string s;
+        bool checkIfItsMyFile(string str) {
+            std::cout << str;
+            return true;
+            }
 
-        std::getline(ifile, s);
-        checkIfItsMyFile(s);
-    }
+        void inputCheck(string file) {
+            std::ifstream ifile;
+            ifile.open(file);
 
-public:
-    void givePassword()
-    {
-        string temp;
-        string password;
+            string s;
 
-        inputCheck(pathOfPassFile);
+            std::getline(ifile, s);
+            checkIfItsMyFile(s);
+            }
 
-        std::cout << "Hello" << std::endl
-                  << "Peazz previde a password: " << std::endl;
-        std::cin >> password;
+        public:
+        void givePassword() {
+            string temp;
+            string password;
 
-        Encripsion.openATheFileWithPassword();
-    }
+            inputCheck(pathOfPassFile);
 
-    FileHandle(string path)
-    {
-        pathOfPassFile = path;
-    }
-};
+            std::cout << "Hello" << std::endl
+                << "Peazz previde a password: " << std::endl;
+            std::cin >> password;
+            std::cout << password << std::endl;
+            password = Encripsion::hash_string(password.c_str());
+            std::cout << password << std::endl;
+            Encripsion::openATheFileWithPassword();
 
-int main(int argc, char const *argv[])
-{
+            }
+
+        FileHandle(string path) {
+            pathOfPassFile = path;
+            }
+    };
+
+int main(int argc, char const* argv[]) {
 
     if (argc != 2)
-    {
+        {
         std::cout << "Pleaz give path"
-                  << "\n";
+            << "\n";
         return 0;
-    }
+        }
 
     FileHandle fileHandle(argv[1]);
 
+fileHandle.givePassword();
+
     std::cout << argc << "\n";
     return 0;
-}
+    }
